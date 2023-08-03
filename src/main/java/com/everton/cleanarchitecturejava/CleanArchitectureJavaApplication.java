17f25d9e1b73425ecdfd4f42dce010aed93d9077
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2023-08-03 16:47:07 -0300

    projeto inicial

---
+++ src/main/java/com/everton/cleanarchitecturejava/CleanArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package com.everton.cleanarchitecturejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureJavaApplication.class, args);
+	}
+
+}
